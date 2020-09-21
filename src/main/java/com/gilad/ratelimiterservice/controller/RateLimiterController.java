a63fe24caf4975e8cc411c24cd8534962670d0e7
Gilad <abudig@post.bgu.ac.il>
2020-09-21 11:49:35 +0300

    update RateLimiterController

--- src/main/java/com/gilad/ratelimiterservice/controller/RateLimiterController.java
+++ src/main/java/com/gilad/ratelimiterservice/controller/RateLimiterController.java
@@ -3,6 +3,7 @@ package com.gilad.ratelimiterservice.controller;
 
 import com.gilad.ratelimiterservice.dao.ReportEntity;
 import com.gilad.ratelimiterservice.dao.Response;
+import com.gilad.ratelimiterservice.service.RateLimiter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,13 @@ import java.net.UnknownHostException;
 
 @RestController
 public class RateLimiterController {
+    private RateLimiter rateLimiter = new RateLimiter();
 
-//TODO - not finish yet
     @PostMapping("/report")
     public Response response(@RequestBody ReportEntity url, HttpServletRequest request) throws UnknownHostException {
         String userIP = getIP(request.getRemoteAddr());
-        return null;
+        //System.out.println("POST from ip: " + userIP + ", url: "+url.getUrl());
+        return new Response(rateLimiter.addRequest(userIP+" "+url.getUrl()));
     }
 
     public String getIP(String ip) throws UnknownHostException {
